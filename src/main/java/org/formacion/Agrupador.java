ddfa49052a17c2e4387bc1bb6d1624a4ee5f9fa7
ITRESOURCES\Guido_Barra <guido_barra@itrsa.com.ar>
2020-08-16 23:32:12 -0300

    add test

---
+++ src/main/java/org/formacion/Agrupador.java
@@ -0,0 +1,4 @@
+package org.formacion;
+
+public class Agrupador {
+}
---
+++ src/test/java/org/formacion/TestAgrupador.java
@@ -0,0 +1,16 @@
+package org.formacion;
+
+import org.junit.Test;
+
+import org.junit.Assert;
+
+public class TestAgrupador {
+
+	@Test
+	public void test() {
+
+		Assert.assertEquals(1, 1);
+		
+	}
+
+}
